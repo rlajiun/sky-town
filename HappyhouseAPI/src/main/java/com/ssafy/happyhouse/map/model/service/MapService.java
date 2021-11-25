ab4f1e54b50867af34e93fb7f02013bf5babd073
rlajiun <rlajiun@kakao.com>
2021-11-25 17:36:33 +0900

    update

--- HappyhouseAPI/src/main/java/com/ssafy/happyhouse/map/model/service/MapService.java
+++ HappyhouseAPI/src/main/java/com/ssafy/happyhouse/map/model/service/MapService.java
@@ -1,10 +1,8 @@
 package com.ssafy.happyhouse.map.model.service;
 
-import java.sql.SQLException;
 import java.util.List;
 
 import com.ssafy.happyhouse.apt.model.AptInfo;
-import com.ssafy.happyhouse.apt.model.AptInfoBasic;
 import com.ssafy.happyhouse.map.model.Zone;
 import com.ssafy.happyhouse.map.model.ZoneChild;
 
@@ -16,6 +14,4 @@ public interface MapService {
 
 	List<AptInfo> getAptInDong(String dong) throws Exception;
 	
-	List<AptInfoBasic> getAptList(String parent) throws SQLException;
-
 }
