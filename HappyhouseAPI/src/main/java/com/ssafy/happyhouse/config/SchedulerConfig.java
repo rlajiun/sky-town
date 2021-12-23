ee963ed6d371356ba0527f01b5fb14fddc75ca3a
rlajiun <rlajiun@kakao.com>
2021-12-23 23:42:10 +0900

    주석 수정

--- HappyhouseAPI/src/main/java/com/ssafy/happyhouse/config/SchedulerConfig.java
+++ HappyhouseAPI/src/main/java/com/ssafy/happyhouse/config/SchedulerConfig.java
@@ -20,7 +20,13 @@ import com.ssafy.happyhouse.apt.model.service.AptService;
 import com.ssafy.happyhouse.apt.util.AptDealSaxParser;
 import com.ssafy.happyhouse.apt.util.AptSaxParser;
 
-@EnableAsync // 추가
+/**
+ * 정해진 시간마다 공공데이터 API에서 데이터를 받아 파싱해 데이터베이스에 저장하는 스케줄러 클래스
+ * 
+ * @author Kim Jiun
+ *
+ */
+@EnableAsync
 @Component
 public class SchedulerConfig {
 	private final static String serviceKey = "oymQn0W8EVpAimvw4XJZnAIxO9Fsc6%2FmQ1sKJ%2FEa%2F9FgjfJFi3Ry%2FMAPq1Iv8AtlAzlA0Wn%2FQVZ1DtjI35ntQg%3D%3D";
@@ -28,55 +34,61 @@ public class SchedulerConfig {
 	private AptService aptService;
 	private int totalCount = 1;
 	private List<AptInfo> aptList;
-	
+
 //	@Scheduled(fixedDelay = 1000*60*60*24) 
 	public void aptDealUpdateTask() throws Exception {
-		System.out.println("시작");
 		List<String> codes = aptService.getGugunCodeList();
 		int mon = 11;
-		
+
 		for (int i = 0; i < codes.size(); i++) {
 			for (int k = mon; k > mon - 6; k--) {
 				for (int j = 1; j <= totalCount / 100 + 1; j++) {
-					StringBuilder urlBuilder = new StringBuilder("http://openapi.molit.go.kr/OpenAPI_ToolInstallPackage/service/rest/RTMSOBJSvc/getRTMSDataSvcAptTradeDev"); /*URL*/
-					urlBuilder.append("?" + URLEncoder.encode("serviceKey", "UTF-8") + "=" + serviceKey); /* Service Key */
-					urlBuilder.append("&" + URLEncoder.encode("pageNo","UTF-8") + "=" + URLEncoder.encode(Integer.toString(j), "UTF-8")); /*페이지번호*/
-					urlBuilder.append("&" + URLEncoder.encode("numOfRows","UTF-8") + "=" + URLEncoder.encode("100", "UTF-8")); /*한 페이지 결과 수*/
-					urlBuilder.append("&" + URLEncoder.encode("LAWD_CD","UTF-8") + "=" + URLEncoder.encode(codes.get(i), "UTF-8")); /*지역코드*/
-					urlBuilder.append("&" + URLEncoder.encode("DEAL_YMD","UTF-8") + "=" + URLEncoder.encode("2021"+Integer.toString(k), "UTF-8")); /*계약월*/
+					StringBuilder urlBuilder = new StringBuilder(
+							"http://openapi.molit.go.kr/OpenAPI_ToolInstallPackage/service/rest/RTMSOBJSvc/getRTMSDataSvcAptTradeDev"); /*
+																																		 * URL
+																																		 */
+					urlBuilder.append(
+							"?" + URLEncoder.encode("serviceKey", "UTF-8") + "=" + serviceKey); /* Service Key */
+					urlBuilder.append("&" + URLEncoder.encode("pageNo", "UTF-8") + "="
+							+ URLEncoder.encode(Integer.toString(j), "UTF-8")); /* 페이지번호 */
+					urlBuilder.append("&" + URLEncoder.encode("numOfRows", "UTF-8") + "="
+							+ URLEncoder.encode("100", "UTF-8")); /* 한 페이지 결과 수 */
+					urlBuilder.append("&" + URLEncoder.encode("LAWD_CD", "UTF-8") + "="
+							+ URLEncoder.encode(codes.get(i), "UTF-8")); /* 지역코드 */
+					urlBuilder.append("&" + URLEncoder.encode("DEAL_YMD", "UTF-8") + "="
+							+ URLEncoder.encode("2021" + Integer.toString(k), "UTF-8")); /* 계약월 */
 					URL url = new URL(urlBuilder.toString());
 					HttpURLConnection conn = (HttpURLConnection) url.openConnection();
 					conn.setRequestMethod("GET");
 					conn.setRequestProperty("Content-type", "application/json");
 					System.out.println("Response code: " + conn.getResponseCode());
 					InputStream is;
-					if(conn.getResponseCode() >= 200 && conn.getResponseCode() <= 300) {
+					if (conn.getResponseCode() >= 200 && conn.getResponseCode() <= 300) {
 						is = conn.getInputStream();
 						AptDealSaxParser parser = new AptDealSaxParser(is);
 						List<AptDeal> aptDeal = parser.getAptDealList();
 						if (aptDeal != null && aptDeal.size() > 0) {
 							System.out.println(aptDeal);
 							aptService.insertAptDealList(aptDeal);
-							System.out.println("부분 완료");
 						}
 						totalCount = parser.getTotalCount();
 					} else {
 						is = conn.getErrorStream();
 					}
 					is.close();
-					conn.disconnect();			
-					}
+					conn.disconnect();
+				}
 			}
 		}
-		System.out.println("전체 완료");
-    }
+	}
 
 	@Scheduled(cron = "0 0 0 1 * ?", zone = "Asia/Seoul") // 매월 1일 정오에 실행
 	public void aptUpdateTask() throws InterruptedException, IOException {
 		System.out.println("시작");
 		aptList = new ArrayList<AptInfo>();
-		for(int i = 1; i <= totalCount / 1000 + 1; i++) {
-			StringBuilder urlBuilder = new StringBuilder("http://apis.data.go.kr/1613000/AptListService2/getTotalAptList"); /* URL */
+		for (int i = 1; i <= totalCount / 1000 + 1; i++) {
+			StringBuilder urlBuilder = new StringBuilder(
+					"http://apis.data.go.kr/1613000/AptListService2/getTotalAptList"); /* URL */
 			urlBuilder.append("?" + URLEncoder.encode("serviceKey", "UTF-8") + "=" + serviceKey); /* Service Key */
 			urlBuilder.append("&" + URLEncoder.encode("pageNo", "UTF-8") + "="
 					+ URLEncoder.encode(Integer.toString(i), "UTF-8")); /* 페이지번호 */
@@ -104,19 +116,15 @@ public class SchedulerConfig {
 		}
 
 		for (int i = 0; i < aptList.size(); i++) {
-			System.out.println(i + "번째 아파트");
+//			System.out.println(i + "번째 아파트");
 			aptMoreSet(i, aptList.get(i).getAptCode());
-			System.out.println(aptList.get(i));
+//			System.out.println(aptList.get(i));
 		}
 
-//		System.out.println(aptList);
 		try {
 			aptService.insertAptList(aptList); // 데이터베이스한테는 요청
-			System.out.println("아파트 목록 insert 완료");
 			aptService.insertAptInfoList(aptList);
-			System.out.println("아파트 기본 정보 목록 insert 완료");
 			aptService.insertAptDetailList(aptList);
-			System.out.println("아파트 상세 정보 목록 insert 완료");
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
@@ -133,12 +141,10 @@ public class SchedulerConfig {
 		HttpURLConnection infoConn = (HttpURLConnection) infoUrl.openConnection();
 		infoConn.setRequestMethod("GET");
 		infoConn.setRequestProperty("Content-type", "application/json");
-//        System.out.println("Response code: " + infoConn.getResponseCode());
 
 		HttpURLConnection detailConn = (HttpURLConnection) detailUrl.openConnection();
 		detailConn.setRequestMethod("GET");
 		detailConn.setRequestProperty("Content-type", "application/json");
-//        System.out.println("Response code: " + detailConn.getResponseCode());
 
 		InputStream isInfo = null, isDetail = null;
 		if (infoConn.getResponseCode() >= 200 && infoConn.getResponseCode() <= 300
